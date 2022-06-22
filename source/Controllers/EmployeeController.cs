4b30fa9821f546c62aebc033ebe68502c6a58ae7
Alumno Tic <alumnotic@utt-edu.ads>
2022-06-21 18:46:33 -0700

    First commit...

---
+++ source/Controllers/EmployeeController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ControlAccess.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : ControllerBase
+{          
+    [HttpGet]
+    public string Get() => "Just a call";
+}
