92e443a9febddc299e61a03237628a9e26a94f52
Samtray <sammychurro@gmail.com>
2022-06-26 14:32:12 -0700

    Update app.module.ts

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-<<<<<<< Updated upstream
 import { MapComponent } from './components/map/map.component';
 import { CardsEmployeeComponent } from './components/cards-employee/cards-employee.component';
-=======
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
->>>>>>> Stashed changes
+
 
 @NgModule({
   declarations: [
