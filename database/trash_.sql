6330159de2e5b6e7d9873b426f91009064a6797b
Tonatiuh010 <l.tonatiuh010@gmail.com>
2022-07-11 23:26:10 -0700

    Restoring Procedures_functionSQL

--- database/trash_.sql
+++ database/trash_.sql
@@ -21,3 +21,71 @@ select * from employee_accesS_level;
 select * from job;
 select * FROM POSITION;
 SELECT * FROM SHIFT;
+
+/* TEST PROCEDURES */
+
+	SET @RESULT = '';
+	CALL SET_EMPLOYEE(
+			1,
+			'TONATIUH',
+			'LOPEZ RAMIREZ',
+			NULL,
+			NULL,
+			'API_TEST',
+			@RESULT
+	);
+	SELECT @RESULT;
+    
+    SET @RESULT = '';
+	CALL SET_DEPARTAMENT(
+		1,
+		'DEPTO 1',
+		'D1',
+		'API_TEST',
+		@RESULT
+	);
+	SELECT @RESULT;
+    
+    SET @RESULT = '';
+	CALL SET_JOB(
+		1,
+		'JOB',
+		'TEST JOB',
+		'API_TEST',
+		@RESULT
+	);
+	SELECT @RESULT;
+    
+    SET @RESULT = '';
+	CALL SET_CARD(
+		1,
+		'12345',
+		1,
+		'API_TEST',
+		@RESULT
+	);
+	SELECT @RESULT;
+    
+    SET @RESULT = '';
+	CALL SET_CARD_CHECK(		
+		'12345',		
+		'API_TEST',
+		@RESULT
+	);
+	SELECT @RESULT;
+    
+    SET @RESULT = '';
+	CALL SET_ACCESS_EMPLOYEE(		
+		1,		
+		1,
+        'DISABLED',
+		@RESULT
+	);
+	SELECT @RESULT;
+    
+    UPDATE EMPLOYEE_ACCESS_LEVEL SET
+		STATUS = 'ENABLED'
+	WHERE	
+		EMPLOYEE_ID = 1
+	AND ACCESS_LEVEL_ID = 1;
+    
