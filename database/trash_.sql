9c0c0b962e5037c44ee211e71a77fb3f3d9d3b5d
Alumno Develop Tic <alumnodev@utt-edu.ads>
2022-07-12 18:43:55 -0700

    Removing obj directory from git

--- database/trash_.sql
+++ database/trash_.sql
@@ -11,7 +11,7 @@ FROM
 WHERE 
 	CC.CARD_ID = C.CARD_ID
 	AND E.EMPLOYEE_ID = C.EMPLOYEE_ID;
-    
+	
 SELECT * from access_level;
 SELECT * FROM CARD;
 SELECT * from card_check;
@@ -21,71 +21,3 @@ select * from employee_accesS_level;
 select * from job;
 select * FROM POSITION;
 SELECT * FROM SHIFT;
-
-/* TEST PROCEDURES */
-
-	SET @RESULT = '';
-	CALL SET_EMPLOYEE(
-			1,
-			'TONATIUH',
-			'LOPEZ RAMIREZ',
-			NULL,
-			NULL,
-			'API_TEST',
-			@RESULT
-	);
-	SELECT @RESULT;
-    
-    SET @RESULT = '';
-	CALL SET_DEPARTAMENT(
-		1,
-		'DEPTO 1',
-		'D1',
-		'API_TEST',
-		@RESULT
-	);
-	SELECT @RESULT;
-    
-    SET @RESULT = '';
-	CALL SET_JOB(
-		1,
-		'JOB',
-		'TEST JOB',
-		'API_TEST',
-		@RESULT
-	);
-	SELECT @RESULT;
-    
-    SET @RESULT = '';
-	CALL SET_CARD(
-		1,
-		'12345',
-		1,
-		'API_TEST',
-		@RESULT
-	);
-	SELECT @RESULT;
-    
-    SET @RESULT = '';
-	CALL SET_CARD_CHECK(		
-		'12345',		
-		'API_TEST',
-		@RESULT
-	);
-	SELECT @RESULT;
-    
-    SET @RESULT = '';
-	CALL SET_ACCESS_EMPLOYEE(		
-		1,		
-		1,
-        'DISABLED',
-		@RESULT
-	);
-	SELECT @RESULT;
-    
-    UPDATE EMPLOYEE_ACCESS_LEVEL SET
-		STATUS = 'ENABLED'
-	WHERE	
-		EMPLOYEE_ID = 1
-	AND ACCESS_LEVEL_ID = 1;
-    
